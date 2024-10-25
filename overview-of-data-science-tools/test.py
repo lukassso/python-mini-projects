463c3a4c704ed2de0d4454cc336576aec43fd4c9
Lukasz <lukasz.zatyka@gmail.com>
2024-10-25 22:11:21 +0200

    Study notes

--- overview-of-data-science-tools/test.py
+++ overview-of-data-science-tools/test.py
@@ -2,8 +2,13 @@
 # A number is even if division by 2 gives a remainder of 0.
 # If the remainder is 1, it is an odd number.
 
-num = int(input("Enter a number: "))
-if (num % 2) == 0:
-   print("{0} is Even".format(num))
-else:
-   print("{0} is Odd".format(num))
+# num = int(input("Enter a number: "))
+# if (num % 2) == 0:
+#    print("{0} is Even".format(num))
+# else:
+#    print("{0} is Odd".format(num))
+
+print("Before input")
+x = int(input("Write some number: "))
+print(f"x is: {x}")
+print(f"number {x} is" + " not"*(not x % 2 == 0) + " even")
